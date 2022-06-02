3bb40e6f5658b58b88a19af54b1f1d7fc57abf23
JasonFang99 <jiashengf@smu.edu>
2022-06-02 15:29:59 -0500

    Rewrite handleChange

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component, PureComponent, useState, useEffect } from 'react'
-import { Form, Input, InputNumber, Radio, Modal, Cascader ,Tree} from 'antd'
+// import { Form, Input, InputNumber, Radio, Modal, Cascader ,Tree} from 'antd'
 import axios from 'axios'
 import Plot from 'react-plotly.js';
 
@@ -10,6 +10,10 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { FormControlLabel, RadioGroup } from '@mui/material';
+import FormLabel from '@mui/material/FormLabel';
+import Radio from '@mui/material/Radio';
+
 
 const option_url = '/voyage/' + '?hierarchical=false'
 
@@ -45,6 +49,7 @@ var scatter_plot_y_vars=[
   'voyage_crew__crew_first_landing'
 ]
 
+
 function App () {
   
   const [plot_field, setarrx] = useState([])
@@ -54,23 +59,52 @@ function App () {
 
   const [option_field, setField] = React.useState(scatter_plot_x_vars[0]);
   const [option_value, setValue] = React.useState(scatter_plot_y_vars[1]);
+  const [option, setOption] = useState({
+    field: scatter_plot_x_vars[0],
+    value: scatter_plot_y_vars[1]
+  })
+
+  const [aggregation, setAgg] = React.useState('sum');
+
+  // const [option_agg, setAgg] = Reacct.
   // const [value, setField] = React.useState('');
 
-  const handleChange_X = (event) => {
-    setField(event.target.value);
-  };
-  const handleChange_Y = (event) => {
-    setValue(event.target.value);
+  // const handleChange = (event) => {
+  //   setAgg({
+  //     ...aggregation,
+  //     [event.target.name]: event.target.checked,
+  //   });
+  // }
+
+  const {sum, average} = aggregation;
+
+  const handleChange_agg = (event) => {
+    
   };
 
+  const handleChange = (event) => {
+    console.log("before", option)
+    console.log("target", event.target)
+    setOption({
+      ...option,
+      [event.target.name]:event.target.value,
+    })
+    console.log("after", option)
+  }
 
-  // function handleClick() {
+  // const handleChange_X = (event) => {
+  //   setField(event.target.value);
+  // };
+  // const handleChange_Y = (event) => {
+  //   setValue(event.target.value);
+  // };
+
 
     // axios.defaults.headers.post['Content-Type'] = 'application/json';
     // axios.defaults.headers.post['Content-Type'] = 'text/plain';
     useEffect(() => {
-      var group_by = option_field//'voyage_itinerary__imp_principal_place_of_slave_purchase__geo_location__name'
-      var value = option_value//"voyage_slaves_numbers__imp_total_num_slaves_disembarked"
+      var group_by = option.field
+      var value = option.value
   
       var data = new FormData();
       data.append('hierarchical', 'False');
@@ -85,9 +119,7 @@ function App () {
   
           setarrx(Object.keys(response.data[value]))
           setarry(Object.values(response.data[value]))
-          // var arr = response.data[value].map((n) => {
-          //   return n
-          // });
+
           console.log(plot_value)
           
         })
@@ -95,34 +127,7 @@ function App () {
           console.log(error);
         });
 
-    }, [option_field, option_value]);
-
-    // var group_by = option_field//'voyage_itinerary__imp_principal_place_of_slave_purchase__geo_location__name'
-    // var value = option_value//"voyage_slaves_numbers__imp_total_num_slaves_disembarked"
-
-    // var data = new FormData();
-    // data.append('hierarchical', 'False');
-
-    // data.append('groupby_fields', group_by)
-    // data.append('groupby_fields', value)
-    // data.append('agg_fn','sum')
-    // data.append('cachename','voyage_export')
-
-    // axios.post('/voyage/groupby', data=data)
-    //   .then(function (response) {
-
-    //     setarrx(Object.keys(response.data[value]))
-    //     setarry(Object.values(response.data[value]))
-    //     // var arr = response.data[value].map((n) => {
-    //     //   return n
-    //     // });
-    //     console.log(plot_value)
-        
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   });
-
+    }, [option.field, option.value]);
 
     // fetch('https://voyages3-api.crc.rice.edu/voyage/', {
     //   method: 'POST',
@@ -149,9 +154,10 @@ function App () {
               <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                value={option_field}
-                label="Field"
-                onChange={handleChange_X}
+                value={option.field}
+                label="field"
+                onChange={handleChange}
+                name="field"
               >
                 {scatter_plot_x_vars.map((option) => (
                   <MenuItem value={option}>
@@ -168,9 +174,10 @@ function App () {
               <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                value={option_value}
-                label="Value Field"
-                onChange={handleChange_Y}
+                value={option.value}
+                label="value"
+                name="value"
+                onChange={handleChange}
               >
                 {scatter_plot_y_vars.map((option) => (
                   <MenuItem value={option}>
@@ -183,9 +190,21 @@ function App () {
             </FormControl>
           </Box>
         </div>
-        {/* <div className='button_container'>
-          <button className='button' onClick={handleClick}>Click Me</button>
+        {/* <div>
+          <FormControl>
+            <FormLabel id="demo-controlled-radio-buttons-group">Aggregation Function</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-controlled-radio-buttons-group"
+              name="controlled-radio-buttons-group"
+              value={value}
+              onChange={handleChange}
+            >
+              <FormControlLabel value="sum" control={<Radio />} label="Sum" />
+              <FormControlLabel value="average" control={<Radio />} label="Average" />
+            </RadioGroup>
+          </FormControl>
         </div> */}
+      
         <div>
           <Plot
             data={[
@@ -202,8 +221,6 @@ function App () {
           />
         </div>
       </div>
-      
-      
     )
   
 
