2ed26f760f905e804a2d7129bf44270ccfa5854b
JasonFang99 <jiashengf@smu.edu>
2022-06-02 13:39:49 -0500

    Add dropdown selection

--- src/App.js
+++ src/App.js
@@ -5,38 +5,81 @@ import { Form, Input, InputNumber, Radio, Modal, Cascader ,Tree} from 'antd'
 import axios from 'axios'
 import Plot from 'react-plotly.js';
 
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 const option_url = '/voyage/' + '?hierarchical=false'
 
+const AUTH_TOKEN = 'Token 30da72e68ebc2beeaaf69a15dcfc56844fb7e05d';
 
-// var plot_field = []
-// var plot_value = []
-
-
+axios.defaults.baseURL = 'https://voyages3-api.crc.rice.edu'; //'http://127.0.0.1:8000'
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
 function App () {
   
   const [plot_field, setarrx] = useState([])
   const [plot_value, setarry] = useState([])
 
+  // const [option_field, setOption] = useState([])
+
+  const [option_field, setField] = React.useState('');
+  const [option_value, setValue] = React.useState('');
+  // const [value, setField] = React.useState('');
+
+  const handleChange_X = (event) => {
+    setField(event.target.value);
+  };
+  const handleChange_Y = (event) => {
+    setValue(event.target.value);
+  };
+
+  var scatter_plot_x_vars=[
+    'voyage_dates__imp_arrival_at_port_of_dis_yyyy',
+    'voyage_dates__imp_length_home_to_disembark',
+    'voyage_dates__length_middle_passage_days',
+    'voyage_crew__crew_voyage_outset',
+    'voyage_crew__crew_first_landing',
+    'voyage_slaves_numbers__imp_total_num_slaves_embarked',
+    'voyage_slaves_numbers__imp_total_num_slaves_disembarked'
+    ]
+  
+  var scatter_plot_y_vars=[
+    'voyage_slaves_numbers__imp_total_num_slaves_embarked',
+    'voyage_slaves_numbers__imp_total_num_slaves_disembarked',
+    'voyage_slaves_numbers__percentage_female',
+    'voyage_slaves_numbers__percentage_male',
+    'voyage_slaves_numbers__percentage_child',
+    'voyage_slaves_numbers__percentage_men_among_embarked_slaves',
+    'voyage_slaves_numbers__percentage_women_among_embarked_slaves',
+    'voyage_slaves_numbers__imp_mortality_ratio',
+    'voyage_slaves_numbers__imp_jamaican_cash_price',
+    'voyage_slaves_numbers__percentage_boys_among_embarked_slaves',
+    'voyage_slaves_numbers__percentage_girls_among_embarked_slaves',
+    'voyage_ship__tonnage_mod',
+    'voyage_crew__crew_voyage_outset',
+    'voyage_crew__crew_first_landing'
+  ]
+
+  // axios.options()
+
+
   function handleClick() {
 
-    const AUTH_TOKEN = 'Token 0bfda2118118484d52aeec86812269aadeb37c67';
-
-    axios.defaults.baseURL = 'https://voyages3-api.crc.rice.edu'; //'http://127.0.0.1:8000'
-    axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
     // axios.defaults.headers.post['Content-Type'] = 'application/json';
     // axios.defaults.headers.post['Content-Type'] = 'text/plain';
 
-
-    var group_by = 'voyage_itinerary__imp_principal_place_of_slave_purchase__geo_location__name'
-    var value = "voyage_slaves_numbers__imp_total_num_slaves_disembarked"
+    var group_by = option_field//'voyage_itinerary__imp_principal_place_of_slave_purchase__geo_location__name'
+    var value = option_value//"voyage_slaves_numbers__imp_total_num_slaves_disembarked"
 
     var data = new FormData();
     data.append('hierarchical', 'False');
 
     data.append('groupby_fields', group_by)
-    data.append('value_field_tuple', value)
-    data.append('value_field_tuple','sum')
+    data.append('groupby_fields', value)
+    data.append('agg_fn','sum')
     data.append('cachename','voyage_export')
 
     axios.post('/voyage/groupby', data=data)
@@ -71,11 +114,50 @@ function App () {
     //   });
   }
 
-
-
-
     return (
       <div>
+        <div>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">X Field</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={option_field}
+                label="Field"
+                onChange={handleChange_X}
+              >
+                {scatter_plot_x_vars.map((option) => (
+                  <MenuItem value={option}>
+                    {option}
+                  </MenuItem>
+                ))}
+                {/* <MenuItem value={scatter_plot_x_vars}>{scatter_plot_x_vars}</MenuItem> */}
+
+              </Select>
+            </FormControl>
+          </Box>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Y Field</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={option_value}
+                label="Value Field"
+                onChange={handleChange_Y}
+              >
+                {scatter_plot_y_vars.map((option) => (
+                  <MenuItem value={option}>
+                    {option}
+                  </MenuItem>
+                ))}
+                {/* <MenuItem value={scatter_plot_x_vars}>{scatter_plot_x_vars}</MenuItem> */}
+
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
         <div className='button_container'>
           <button className='button' onClick={handleClick}>Click Me</button>
         </div>
